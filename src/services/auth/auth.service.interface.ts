ce07cd5d976a73d0c2982872c913ebaf996ac701
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-08-15 11:48:12 +0100

    refactor: Update IAuthService interface to include resetPassword method with typed parameter

--- src/services/auth/auth.service.interface.ts
+++ src/services/auth/auth.service.interface.ts
@@ -3,12 +3,13 @@ import { LoginResponseDto } from "../../dtos/authDtos/loginResponse.dto";
 import { ForgotPasswordRequestDto } from '../../dtos/authDtos/forgotPasswordRequest.dto';
 import { EmailVerificationRequestDto } from "../../dtos/authDtos/emailVerificationRequest.dto";
 import { VerifyEmailRequestDto } from "../../dtos/authDtos/verifyEmailRequest.dto";
+import { ResetPasswordRequestDto } from "../../dtos/authDtos/resetPasswordRequest.dto";
 
 export interface IAuthService {
     login(loginData: LoginRequestDto): Promise<LoginResponseDto>;
     register(registerData: any): Promise<boolean>;
     forgotPassword(forgotPasswordData: ForgotPasswordRequestDto): Promise<boolean>;
-    resetPassword(resetPasswordData: any): Promise<boolean>;
+    resetPassword(resetPasswordData: ResetPasswordRequestDto): Promise<boolean>;
     emailVerification(emailVerificationData: EmailVerificationRequestDto): Promise<boolean>;
     verifyEmail(verifyEmailData: VerifyEmailRequestDto): Promise<boolean>;
 }
